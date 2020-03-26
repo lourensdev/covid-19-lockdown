edfa5237bad080c61bcdccb7a0c507efbd8d21a5
Lourens de Villiers <lpdevilliers@gmail.com>
2020-03-26 21:34:52 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+Vue.component("timer-item", {
+  props: ["text", "desc"],
+  template: "<div class='b-timer-item'><span class='e-timer-value'>{{text}}</span><span class='e-timer-desc'>{{desc}}</span></div>"
+});
+
+function getCountdown() {
+  return moment().countdown("2020-4-17");
+}
+
+function padTime(time) {
+  if (time < 10) {
+    return `0${time}`;
+  }
+  return time.toString();
+}
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    days: padTime(getCountdown().days),
+    hours: padTime(getCountdown().hours),
+    minutes: padTime(getCountdown().minutes),
+    seconds: padTime(getCountdown().seconds)
+  }
+});
+
+setInterval(() => {
+  app.days = padTime(getCountdown().days);
+  app.hours = padTime(getCountdown().hours);
+  app.minutes = padTime(getCountdown().minutes);
+  app.seconds = padTime(getCountdown().seconds);
+}, 1000);
